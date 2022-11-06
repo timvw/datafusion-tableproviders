be53469731e1624315edd5e42eddd29ff0fb4c72
Tim Van Wassenhove <tim@timvw.be>
2022-11-06 20:30:40 +0100

    more work on example

--- examples/demo.rs
+++ examples/demo.rs
@@ -4,15 +4,14 @@ use datafusion::common::Result;
 use datafusion::datasource::datasource::TableProviderFactory;
 use datafusion::datasource::TableProvider;
 
-use datafusion_tableproviders::delta::DeltaTableProviderFactory;
+use datafusion_tableproviders::*;
 
 #[tokio::main]
 async fn main() -> Result<()> {
 
     let mut ctx = SessionContext::new();
 
-    let delta_table_provider_factory = DeltaTableProviderFactory{};
-    let _ = ctx.table_factories.insert(String::from("DELTA"), Arc::new(delta_table_provider_factory));
+    ctx = register_all_enabled_factories(ctx)?;
 
     let _ = ctx
         .sql("CREATE EXTERNAL TABLE x STORED AS DELTA LOCATION './testing'")
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,17 @@
+use std::sync::Arc;
+use datafusion::prelude::*;
+use datafusion::common::Result;
+use crate::delta::DeltaTableProviderFactory;
+
 #[cfg(feature = "delta")]
 pub mod delta;
+
+pub fn register_all_enabled_factories(mut ctx: SessionContext) -> Result<SessionContext> {
+
+    if cfg!(feature = "delta") {
+        let delta_table_provider_factory = DeltaTableProviderFactory{};
+        let _ = ctx.table_factories.insert(String::from("DELTA"), Arc::new(delta_table_provider_factory));
+    }
+
+    Ok(ctx)
+}
